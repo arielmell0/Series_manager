de53f9cae365b75d9690267e5554f8704ec2d38d
arielmell0 <arielmello@rede.ulbra.br>
2020-11-29 16:56:38 -0300

    array series and routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ola', function () {
     echo 'hello, world!';
 });
+
+Route::get('/series', function () {
+    $series = [
+        'Grey\'s Anatomy',
+        'Lost',
+        'Agents of SHIELD'
+    ];
+
+    $html = "<ul>";
+    foreach ($series as $serie){
+        $html .="<li>$serie</li>";
+    }
+    $html .= "</ul>";
+
+    return $html;
+});
+
